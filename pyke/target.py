35f62b009923680ce0ca1d495c8880d847ca404e
Mike Lowen <mike@mlowen.com>
2012-09-12 23:19:16 +1200

    Implemented the set_source_regex method in the target.Config class.

--- pyke/target.py
+++ pyke/target.py
@@ -1,18 +1,26 @@
+import re
+
 def get_default_target():
 	return 'default'
 
 class Config:
 	def __init__(self):
 		self.paths = []
-		self.build_system = ''
+		self.regexs = []
 	
-	def set_source_path(paths):
+	# Source Files
+	def set_source_path(self, paths):
 		if isinstance(paths, list):
 			self.paths = [ p for p in paths if isinstance(p, str)]
 		elif isinstance(paths, str):
 			self.paths = [ paths ]
 		else:
-			raise Exception('Invalid object passed to set_source_paths, expecting a string or an array')
+			raise Exception('Invalid argument, expecting a string or a list')
 	
-	def set_build_system(builder):
-		self.build_system = builder
+	def set_source_regex(self, regex):
+		if isinstance(regex, list):
+			self.regexs = [ re.compile(r) for r in regex if isinstance(r, str)]
+		elif isinstance(regex, str):
+			self.regexs = [ re.compile(regex) ]
+		else:
+			raise Exception('Invalid argument, expecting a string or a list')
