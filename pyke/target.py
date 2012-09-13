25ee9495d64ca1ec73282fd3326563e2a3d016d1
Mike Lowen <mike@mlowen.com>
2012-09-13 23:27:44 +1200

    Setting up so pyke can run without been given a build file.

--- pyke/target.py
+++ pyke/target.py
@@ -6,8 +6,8 @@ def get_default_target():
 
 class Config:
 	def __init__(self):
-		self.paths = []
-		self.patterns = []
+		self.paths = None
+		self.patterns = None
 		self.output_path = None
 		self.output_name = None
 	
@@ -20,29 +20,41 @@ class Config:
 		else:
 			raise Exception('Invalid argument, expecting a string or a list')
 	
-	def set_source_regex(self, regex):
-		if isinstance(regex, list):
-			self.patterns = [ r for r in regex if isinstance(r, str)]
-		elif isinstance(regex, str):
-			self.patterns = [ regex ]
+	def get_source_paths(self):
+		if self.paths == None or len(self.paths) == 0:
+			return [ os.path.relpath(os.getcwd()) ]
+		
+		return self.paths
+			
+	def set_source_pattern(self, patterns):
+		if isinstance(patterns, list):
+			self.patterns = [ p for p in patterns if isinstance(p, str)]
+		elif isinstance(patterns, str):
+			self.patterns = [ patterns ]
 		else:
 			raise Exception('Invalid argument, expecting a string or a list')
 	
+	def get_source_patterns(self):
+		if self.patterns == None or len(self.patterns) == 0:
+			return [ '*.cc', '*.cpp', '*.cxx' ]
+		
+		return self.patterns
+			
 	def get_source_files(self):
 		# Parse out the files first
-		source_files = [ f for f in self.paths if os.path.exists(f) and os.path.isfile(f) ]
+		source_files = [ f for f in self.get_source_paths() if os.path.exists(f) and os.path.isfile(f) ]
 		
 		for d in [ f for f in self.paths if os.path.exists(f) and os.path.isdir(f) ]:
-			source_files.extend(self.get_source_in_directory(d))
+			source_files.extend(self.get_source_in_directory(d, self.get_source_patterns()))
 		
 		return source_files
 	
-	def get_source_in_directory(self, directory):
+	def get_source_in_directory(self, directory, patterns):
 		dir_items = os.listdir(directory)
-		source_files = [ os.path.join(directory, f) for f in dir_items if os.path.isfile(os.path.join(directory, f)) and True in [ fnmatchcase(f, p) for p in self.patterns ]]
+		source_files = [ os.path.join(directory, f) for f in dir_items if os.path.isfile(os.path.join(directory, f)) and True in [ fnmatchcase(f, p) for p in patterns ]]
 		
 		for sub_dir in [ d for d in dir_items if os.path.isdir(d) ]:
-			source_files.extend(self.get_source_in_directory(os.path.join(directory, sub_dir)))
+			source_files.extend(self.get_source_in_directory(os.path.join(directory, sub_dir), patterns))
 		
 		return source_files
 	
