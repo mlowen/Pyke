0b7bf756bb3009713aac6da96fc110f0eea430a1
Mike Lowen <mike@mlowen.com>
2012-09-13 22:07:42 +1200

    Implemented the retreival of the source files in the target.Config.

--- pyke/target.py
+++ pyke/target.py
@@ -1,4 +1,5 @@
-import re
+import os
+from fnmatch import fnmatchcase
 
 def get_default_target():
 	return 'default'
@@ -6,7 +7,7 @@ def get_default_target():
 class Config:
 	def __init__(self):
 		self.paths = []
-		self.regexs = []
+		self.patterns = []
 	
 	# Source Files
 	def set_source_path(self, paths):
@@ -19,8 +20,26 @@ class Config:
 	
 	def set_source_regex(self, regex):
 		if isinstance(regex, list):
-			self.regexs = [ re.compile(r) for r in regex if isinstance(r, str)]
+			self.patterns = [ r for r in regex if isinstance(r, str)]
 		elif isinstance(regex, str):
-			self.regexs = [ re.compile(regex) ]
+			self.patterns = [ regex ]
 		else:
 			raise Exception('Invalid argument, expecting a string or a list')
+	
+	def get_source_files(self):
+		# Parse out the files first
+		source_files = [ f for f in self.paths if os.path.exists(f) and os.path.isfile(f) ]
+		
+		for d in [ f for f in self.paths if os.path.exists(f) and os.path.isdir(f) ]:
+			source_files.extend(self.get_source_in_directory(d))
+		
+		return source_files
+	
+	def get_source_in_directory(self, directory):
+		dir_items = os.listdir(directory)
+		source_files = [ os.path.join(directory, f) for f in dir_items if os.path.isfile(os.path.join(directory, f)) and True in [ fnmatchcase(f, p) for p in self.patterns ]]
+		
+		for sub_dir in [ d for d in dir_items if os.path.isdir(d) ]:
+			source_files.extend(self.get_source_in_directory(os.path.join(directory, sub_dir)))
+		
+		return source_files
