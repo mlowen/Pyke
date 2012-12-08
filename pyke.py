65b3fe0456e75bf16bce52ae86b5523492437d67
Mike Lowen <mike@mlowen.com>
2012-12-09 11:27:47 +1300

    Adding some documentation to pyke.py

--- pyke.py
+++ pyke.py
@@ -1,5 +1,12 @@
 #! /bin/env python
 
+'''
+	This script is used for easily testing the local changes that
+	have been made to pyke, which can be useful when you already
+	have a stable version of pyke installed as it means you do not
+	need to uninstall and re-install different version all the time.
+'''
+
 import pyke
 
 if __name__ == '__main__':
