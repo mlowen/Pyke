5ed8f589c30ec2af057c72d5dcbdd76d42c7f1f3
Mike Lowen <mike@mlowen.com>
2013-06-08 14:57:05 +1200

    Added builder factory
    Switched spaces to tabs, added the builder factory which will be used
    going forward to retrieve builders.

--- pyke/builders/__init__.py
+++ pyke/builders/__init__.py
@@ -1,15 +1,36 @@
+from pyke import PykeException
+
 from . import gpp
 
+
 def factory(compiler, output_type):
-    if output_type not in [ 'executable', 'sharedlib', 'dynamiclib' ]:
-        raise Exception('Unknown output type: %s' % output_type)
-    
-    if compiler == 'g++':
-        if output_type == 'executable':
-            return gpp.ExecutableCompiler()
-        elif output_type == 'sharedlib':
-            return gpp.SharedLibraryCompiler()
-        elif output_type == 'dynamiclib':
-            return gpp.DynamicLibraryCompiler()
-    
-    raise Exception('Unknown compiler: %s' % compiler)
+	if output_type not in [ 'executable', 'sharedlib', 'dynamiclib' ]:
+		raise Exception('Unknown output type: %s' % output_type)
+	
+	if compiler == 'g++':
+		if output_type == 'executable':
+			return gpp.ExecutableCompiler()
+		elif output_type == 'sharedlib':
+			return gpp.SharedLibraryCompiler()
+		elif output_type == 'dynamiclib':
+			return gpp.DynamicLibraryCompiler()
+	
+	raise Exception('Unknown compiler: %s' % compiler)
+
+class Factory:
+	def __init__(self):
+		self._builders = {}
+		
+		# Load the built-in builders
+	
+	def get(self, name, output_type):
+		if name not in self._builders:
+			raise PykeException('Unknown builder %s.' % name)
+		
+		factory = self._builders[name]
+		builder = factory(output_type)
+		
+		if builder is None:
+			raise PykeException('No builder for %s, %s combo.' % (name, output_type))
+		
+		return builder
